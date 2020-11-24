12ea13d64fdaf33b405bfaca33de2aaf98e8173c
Adrian Larsson <developmentgitemail@protonmail.com>
2020-11-24 16:08:53 +0100

    Refractor of home page view test

--- lists/tests.py
+++ lists/tests.py
@@ -1,20 +1,16 @@
 from django.test import TestCase
-from lists.views import home_page 
-from django.urls import resolve
-from django.http import HttpRequest 
 # Create your tests here.
 
 class HomePageTest(TestCase):
 
-    def test_root_resolves_to_home_url(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-    
     def test_home_page_return_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request) 
+        response = self.client.get('/')
+
         html = response.content.decode('utf-8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
+        
+        
+        self.assertTemplateUsed(response, 'home.html')
 
