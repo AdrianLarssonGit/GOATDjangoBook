11c6cdb6d3e8eec686845ead9d47a14fae897c20
Adrian Larsson <developmentgitemail@protonmail.com>
2020-11-24 15:04:37 +0100

    FT now checks for to-do list

--- functional_test.py
+++ functional_test.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+
     def tearDown(self):
         self.browser.quit()
     
@@ -15,9 +18,30 @@ class NewVisitorTest(unittest.TestCase):
 
         #She notices the page title and header talking about lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail("Finish the test!")
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn("To-Do", header_text)
+
         
         #She is invited to enter a to do item right 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attributes('placeholder'),
+                'Enter a to-do item')
+
+        #She types buy peacockfeathers
+        inputbox.send_keys('Buy peacock feathers')
+
+        #When she press enter the page updates and the list has saved
+        #first item
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        table = self.browser.find_elements_by_id('id_list_table')
+        rows = tabel.find_element_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: Buy peacock feathers' for row in rows))
+
+        self.fail("Finish the test!")
 
 if __name__ == '__main__':
     unittest.main()
