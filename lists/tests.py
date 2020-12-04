42d4ad34171f6a3d72a1170e71219cdb8df2983f
Adrian Larsson <developmentgitemail@protonmail.com>
2020-12-04 14:28:45 +0100

    Added test, cleaned up HTML

--- lists/tests.py
+++ lists/tests.py
@@ -4,17 +4,30 @@ from .models import Item
 
 class HomePageTest(TestCase):
 
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemy 1")
+        Item.objects.create(text="itemy 2")
+
+        response = self.client.get('/')
+
+        self.assertIn('itemy 1', response.content.decode())
+        self.assertIn('itemy 2', response.content.decode())
+
+
     def test_home_page_return_correct_html(self):
         response = self.client.get('/')
         
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_post_request(self):
-        response = self.client.post('/', data = {'item_text': 'A new list item'})
+        self.client.post('/', data={'item_text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .models import Item
-# Create your views here.
+
 def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
         return redirect('/')
 
-    return render(request, 'home.html')
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
