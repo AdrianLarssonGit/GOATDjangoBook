138ba96a110c58341be1fe1b618ae1d6761f97ad
Adrian Larsson <developmentgitemail@protonmail.com>
2020-12-04 14:42:19 +0100

    Created separate folder for FT, refracted FT with LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,62 +1,55 @@
 from selenium import webdriver
+from django.test import LiveServerTestCase
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
-    
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-
     def test_can_start_a_list_and_retrive_it_later(self):
-        #Edit has heard about a cool new onlint  to do app. 
-        #She goes to check it out
-        self.browser.get('http://localhost:8000')
+        # Edit has heard about a cool new onlint  to do app.
+        # She goes to check it out
+        self.browser.get(self.live_server_url)
 
-        #She notices the page title and header talking about lists
+        # She notices the page title and header talking about lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn("To-Do", header_text)
 
-        
-        #She is invited to enter a to do item right 
+        # She is invited to enter a to do item right
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-                inputbox.get_attribute('placeholder'),
-                'Enter a to-do item')
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item')
 
-        #She types buy peacockfeathers
+        # She types buy peacockfeathers
         inputbox.send_keys('Buy peacock feathers')
 
-        #When she press enter the page updates and the list has saved
-        #first item
+        # When she press enter the page updates and the list has saved
+        # first item
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        #The page once again updates and now she sees both items on the list
+        # The page once again updates and now she sees both items on the list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make fly')
 
-
-
-
         self.fail("Finish the test!")
-
-if __name__ == '__main__':
-    unittest.main()
